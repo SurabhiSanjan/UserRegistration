bbf3996a8143a3614f823a3a4bec9c181c24bed0
surabhisanjan <surabhisanjan@gmail.com>
2022-12-12 17:09:54 +0530

    Ability to test the user first name in User Registration

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
+import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration!!!");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String firstName=sc.next();
+        boolean res= Pattern.matches("^[A-Z][A-Za-z]{2,}$", firstName);
+        if(res) {
+            System.out.println("You have entered valid First Name");
+        }else {
+            System.out.println("Entered First Name is invalid");
+        }
     }
 }
